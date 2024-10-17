514503cf05d48a9739fee10e96cca243fa6809fb
jaideepjaison <jaison3666@gmail.com>
2024-10-17 21:24:57 +0530

    weather_app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState('');
+
+  const handleChange = (event) => {
+      setCity(event.target.value);
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError('');
+    setWeatherData(null);
+      
+    
+    try {
+    
+        const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bf960caa19fce81197c6d7b931ebb206`
+        );
+
+        setWeatherData(response.data);
+    } catch (err) {
+      if(city=='')
+      {
+        setError('Please Enter the City');
+      }else{
+        setError('City not found');
+      }
+    }
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My Weather APP
-        </a>
-      </header>
+    <body>
+    <div class="weather-search-container">
+        <h1>Weather Information</h1>
+        <form id="weatherForm" onSubmit={handleSubmit}>
+            <input type="text" id="cityInput" value={city} onChange={handleChange} placeholder="Enter city name" />
+            <button type="submit">Search</button>
+        </form>
+        <div id="error" class="error"></div>
+        <div id="weatherResult" class="weather-result"></div>
+        <div className='messages'>
+        {error && <p className="error">{error}</p>}
+        {weatherData && (
+            <div className="weather-result">
+                <h2>{weatherData.name}</h2>
+                <p>Temperature: {(weatherData.main.temp - 273.15).toFixed(1)} °C</p>
+                <p>Weather: {weatherData.weather[0].description}</p>
+                <p>Country: {weatherData.sys.country}</p>
+            </div>
+        )}
     </div>
+    </div>
+    
+</body>
+
   );
 }
 
